e3165c0b8e12c8868623ccfc91cee85b2ef300de
Miriam Goren <miriamgoren85@gmail.com>
2020-06-20 23:27:14 +0300

    bug fixes

--- js/scripts.js
+++ js/scripts.js
@@ -8,80 +8,97 @@ function Loader() {
 
 function alertOn() {
   let alert = document.getElementById("alert");
-  alert.style.visibility="visible";
+  alert.style.visibility = "visible";
   let alertBox = document.getElementById("num");
-  alertBox.classList.add("text-danger","border-danger");
+  alertBox.classList.add("text-danger", "border-danger");
 }
 
 function alertOff() {
   let alert = document.getElementById("alert");
-  alert.style.visibility="hidden";
+  alert.style.visibility = "hidden";
   let alertBox = document.getElementById("num");
-  alertBox.classList.remove("text-danger","border-danger");
+  alertBox.classList.remove("text-danger", "border-danger");
 }
-
 
 function Error42() {
   let red = document.getElementById("result");
-  red.classList.add("text-decoration-none","text-danger","font-weight-lighter","small");
-
+  red.classList.add(
+    "text-decoration-none",
+    "text-danger",
+    "font-weight-lighter",
+    "small"
+  );
 }
 
-function resetFunction(){
-
-    var resetResult = document.getElementById("result");
-    if (resetResult.className = "text-danger") {
-      alertOff();
-    }
-   
-
+function resetFunction() {
+  var resetResult = document.getElementById("result");
+  if ((resetResult.className = "text-danger")) {
+    alertOff();
+  }
+  alertOff();
   var resetAlert = document.getElementById("alert");
-    if (resetAlert.style.visibility === "visible") {
-      resetAlert.style.visibility = "invisible";
-    } else {
-      resetAlert.style.visibility = "invisible";
-    }
-
-    var resetNum = document.getElementById("num");
-    if (resetNum.style.color === "#dc3545" ) {
-      resetNum.style.color = "#495057";
-    } else {  
-      resetNum.style.color = "#495057";
-    }
-
-
-  }
-
-function on_click_handler(){
-  var checkbox_elem = document.getElementById("myCheck");
-  let checkbos_is_checked = checkbox_elem.checked;
-  if (checkbos_is_checked){
-    fetch_data_from_server_ex5();
+  if (resetAlert.style.visibility === "visible") {
+    resetAlert.style.visibility = "invisible";
   } else {
-    fetch_data_from_server_ex4();
+    resetAlert.style.visibility = "invisible";
   }
-}
-  function fetch_data_from_server_ex4()
-  {
-    console.log("ex4")
-      number = document.getElementById("num").value;
-      url = "http://127.0.0.1:5050/fibonacci/";
-      url_with_data = url+number;
-      console.log(url_with_data);
-      fetch(url_with_data)
-          .then(response => response.json())
-          .then(response => document.getElementById("result").innerHTML = response["result"])
+
+  var resetNum = document.getElementById("num");
+  if (resetNum.style.color === "#dc3545") {
+    resetNum.style.color = "#495057";
+  } else {
+    resetNum.style.color = "#495057";
   }
+  resetNum.style.color = "#495057";
   
-function fetch_data_from_server_ex5() {
+}
+
+function on_click_handler() {
+  resetFunction();
   Loader();
-  console.log("ex5");
+  document.getElementById("result").innerHTML = " "
   number = document.getElementById("num").value;
   if (number > 50) {
     Loader();
     alertOn();
     return;
   }
+  var checkbox_elem = document.getElementById("myCheck");
+  let checkbox_is_checked = checkbox_elem.checked;
+  if (checkbox_is_checked) {
+    fetch_data_from_server_ex5(number);
+  } else {
+    fetch_data_from_server_ex4(number);
+  }
+}
+function fetch_data_from_server_ex4(number) {
+  console.log("ex4");
+  Loader();
+  if (number < 0)
+  {
+    document.getElementById("result").innerHTML = "Error: number cant be smaller than 0"
+    return;
+  }
+  if (number == 42)
+  {
+    document.getElementById("result").innerHTML = "42 is the meaning of life"
+    return;
+  }
+  url = "http://127.0.0.1:5050/fibonacci/";
+  url_with_data = url + number;
+  console.log(url_with_data);
+  fetch(url_with_data)
+    .then((response) => response.json())
+    .then(
+      (response) =>
+        (document.getElementById("result").innerHTML = response["result"])
+    );
+}
+
+function fetch_data_from_server_ex5(number) {
+  
+  console.log("ex5");
+
 
   url = "http://127.0.0.1:5050/fibonacci/";
   url_with_data = url + number;
@@ -113,15 +130,16 @@ window.addEventListener("load", () => {
       response.json().then((jsonData) => {
         let array1 = jsonData.results;
         let correctDate;
-        console.log(array1);
-        array1= array1.sort(function(a, b){return b.createdDate-a.createdDate});
-       
-        let last_element_idx = Math.min(array1.length,5);
-        for (let i=0 ; i < last_element_idx; i++) {
-          correctDate= new Date(array1[i].createdDate).toString();
+        array1 = array1.sort(function (a, b) {
+          return b.createdDate - a.createdDate;
+        });
+
+        let last_element_idx = Math.min(array1.length, 5);
+        for (let i = 0; i < last_element_idx; i++) {
+          correctDate = new Date(array1[i].createdDate).toString();
           const li = document.createElement("li");
-          li.className='list-group-item';
-          li.style.border.color='black';
+          li.className = "list-group-item";
+          li.style.border.color = "black";
           resultsList.appendChild(
             li
           ).innerText = `The Fibonacci of ${array1[i].number} is ${array1[i].result} Calculated at ${correctDate}.`;
